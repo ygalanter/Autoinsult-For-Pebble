4455435b2fc4c83f7e24fbdf252e29a316f18e83
ygalanter <yuri@galanter.net>
2014-10-24 10:01:53 -0400

    Initial push

---
+++ src/app.js
@@ -0,0 +1,53 @@
+/* Main aoo module */
+
+var UI = require('ui');
+var Insult = require('insult');
+
+var iInsultType;
+
+// main insult card to display insults
+var insultCard = new UI.Card({
+  title: 'Insult',
+  /* action: {
+    select: 'images/refresh.png',
+    backgroundColor: 'white'
+  }, */
+  scrollable: true
+});
+
+// on clicking "select" on the card regenerate the insult  
+insultCard.on('click', function(e) {
+  if (e.button == 'select') {
+    insultCard.body('Please wait to be insulted...');
+    Insult.generateInsult(iInsultType, displayInsult);
+  } 
+});
+
+// callback funtion that displays generated insult
+function displayInsult(i_sInsultText){
+   insultCard.body(i_sInsultText + '\n\n([select] for new)');
+}
+
+
+// creating selection menu
+var typeMenu = new UI.Menu({
+  sections: [{
+    title: 'Select insult style',
+    items:[
+      {title:'Arabian'},
+      {title:'Shakespearean'},      
+      {title:'Mediterranean'},      
+      {title:'Modern'}      
+    ]
+  }]
+});
+
+// on clicking "select" on menu generate the insult  
+typeMenu.on('select', function(e) {
+  iInsultType = e.itemIndex;
+  insultCard.body('Please wait to be insulted...');
+  insultCard.show();  
+  Insult.generateInsult(iInsultType, displayInsult);
+});
+
+typeMenu.show();
